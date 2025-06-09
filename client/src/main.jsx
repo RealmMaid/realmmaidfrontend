2b44aa25720c30c7fe573df51caccc51be8e07fa
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-09 10:39:41 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AuthProvider } from './hooks/useAuth.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './contexts/CartContext.jsx';
-// --- NEW: Import the WebSocketProvider ---
-import { WebSocketProvider } from './contexts/WebSocketProvider.jsx';
-
-console.log("main.jsx: Attempting to render App within all providers...");
+// NOTE: We have temporarily removed your custom providers to find the source of the issue.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        {/* --- NEW: WebSocketProvider wraps the app to provide chat functionality --- */}
-        <WebSocketProvider>
-          <CartProvider>
-              <App />
-          </CartProvider>
-        </WebSocketProvider>
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
