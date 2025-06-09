800ab0914201be4a6a8e6a2441c0593d23b3ccb9
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-09 10:48:00 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth.jsx';
-// NOTE: We are re-introducing AuthProvider to test it.
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext.jsx';
+import { WebSocketProvider } from './contexts/WebSocketProvider.jsx';
 
+// This is the entry point of your application, wrapping the App
+// component with all the necessary providers for routing, authentication,
+// shopping cart state, and WebSocket connections.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <WebSocketProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </WebSocketProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
