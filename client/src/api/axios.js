c0f713971a2a9fc89dd8885b95b389cb4b838c33
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-10 03:19:47 -0700

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,40 @@
 // client/src/api/axios.js
 import axios from 'axios';
+import { getCsrfToken } from './csrf'; // Import our new promise function
 
-// The base URL now includes /api, pointing directly to our backend's API root.
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: true, // This is super important for sending session cookies!
+    baseURL: API_BASE_URL,
+    withCredentials: true,
 });
+
+// This is our super-smart interceptor!
+API.interceptors.request.use(
+    async (config) => {
+        // We usually don't need CSRF for simple GET requests
+        if (config.method === 'get') {
+            return config;
+        }
+
+        try {
+            // This line tells the request to PAUSE and wait for our promise to get the token!
+            const token = await getCsrfToken();
+            if (token) {
+                // Once we have the token, we add it to the header!
+                config.headers['X-CSRF-Token'] = token;
+            }
+        } catch (error) {
+            console.error("CSRF token could not be attached. Aborting request.", error);
+            // If we can't get a token, it's better to cancel the request than have it fail on the server.
+            return Promise.reject(error);
+        }
+    
+        return config;
+    }, 
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default API;
