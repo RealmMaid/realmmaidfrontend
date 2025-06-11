903d915d47cc7b3a3013ecde09d26ec04d7f4200
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 16:52:28 -0700

    Create abilities.js

---
+++ client/src/data/abilities.js
@@ -0,0 +1,23 @@
+export const abilities = [
+  {
+    id: 'slam',
+    name: 'Slam',
+    description: 'Instantly deal damage equal to 30 seconds of your DPS.',
+    cooldown: 90,
+    classId: 'Warrior',
+  },
+  {
+    id: 'arcane_power',
+    name: 'Arcane Power',
+    description: 'For 10s, double all damage & DPS. Clicks may grant Fame instead of damage.',
+    cooldown: 120,
+    classId: 'Wizard',
+  },
+  {
+    id: 'virulent_outbreak',
+    name: 'Virulent Outbreak',
+    description: 'Instantly applies 50 stacks of Poison if Vipers are equipped.',
+    cooldown: 60,
+    classId: 'Sorcerer',
+  },
+];
