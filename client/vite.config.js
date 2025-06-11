35019e0968cde68fcc2ca13c2e4065c90ed2aa80
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 13:12:57 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 1600, }
   // --- PRODUCTION-READY SETUP ---
   // This proxy configuration will forward any requests from your React app
   // that start with '/api' to your backend server running on port 3000.
