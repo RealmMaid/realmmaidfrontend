6012869b657992002361cf1d11ca11f0df92b630
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 11:57:30 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+//import './index.css';
 import { AuthProvider } from './hooks/useAuth.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './contexts/CartContext.jsx';
