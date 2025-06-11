e93169981cd04b6c81cdae82ecd899c4ea2c3213
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 16:54:11 -0700

    Create prestigeUpgrades.js

---
+++ client/src/data/prestigeUpgrades.js
@@ -0,0 +1,5 @@
+export const prestigeUpgrades = [
+    { id: 'permanentDamage', name: 'Exalted Power', description: 'Permanently increase all damage by 10% per level.', cost: 1, type: 'damage_multiplier' },
+    { id: 'permanentFame', name: 'Exalted Wealth', description: 'Start each new run with 1000 Fame per level.', cost: 2, type: 'starting_score' },
+    { id: 'permanentPPS', name: 'Exalted Will', description: 'Permanently gain 50 DPS per level.', cost: 3, type: 'starting_pps' }
+];
