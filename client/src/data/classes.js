7d2bd5c32b39b8fb65b337cd83dd315d7643e7c9
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 16:53:18 -0700

    Create classes.js

---
+++ client/src/data/classes.js
@@ -0,0 +1,5 @@
+export const classes = [
+    { id: 'Warrior', name: 'Warrior', image: '/warrior.png' },
+    { id: 'Wizard', name: 'Wizard', image: '/wizard.png' },
+    { id: 'Sorcerer', name: 'Sorcerer', image: '/sorcerer.png' },
+];
