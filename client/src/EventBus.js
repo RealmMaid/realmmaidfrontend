2ee2a9cfc5d3ccdf25ac6582f0b5583e9546acb8
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 21:44:05 -0700

    Update EventBus.js
    no error 185

--- client/src/EventBus.js
+++ client/src/EventBus.js
@@ -1,7 +1,8 @@
 import Phaser from 'phaser';
 
 // This creates a single, shared instance of Phaser's event emitter.
-// Any part of our app that imports this file will be using the exact same instance.
+// Any part of our app that imports this file will be using the exact same instance,
+// which is crucial for communication.
 const EventBus = new Phaser.Events.EventEmitter();
 
 export default EventBus;
