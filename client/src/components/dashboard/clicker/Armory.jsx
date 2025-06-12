3ba692a7a2fa19f3a70b125c870e536f3b3402a7
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 22:30:44 -0700

    Update Armory.jsx

--- client/src/components/dashboard/clicker/Armory.jsx
+++ client/src/components/dashboard/clicker/Armory.jsx
@@ -1,40 +1,36 @@
 import React from 'react';
-import { useGameStore } from '../../../stores/gameStore';
-import { weapons } from '../../../data/weapons';
+import { useGameStore } from '../../../stores/gameStore.jsx';
+import { weapons } from '../../../data/weapons.js';
 
 export function Armory() {
-    // Get the state and actions related to weapons from our store
+    // Get the DATA we need to display the UI
     const {
         unlockedWeapons,
         equippedWeapon,
-        handleEquipWeapon
     } = useGameStore(state => ({
         unlockedWeapons: state.unlockedWeapons,
         equippedWeapon: state.equippedWeapon,
-        handleEquipWeapon: state.handleEquipWeapon,
     }));
 
     return (
         <div className="upgrades-shop">
             <h4>Armory</h4>
             <div className="armory-grid">
-                {/* The default weapon is always available to equip */}
                 <div
                     key="default"
                     className={`weapon-item ${equippedWeapon === 'default' ? 'equipped' : ''}`}
-                    onClick={() => handleEquipWeapon('default')}
+                    onClick={() => useGameStore.getState().handleEquipWeapon('default')}
                 >
                     <strong>Default Sword</strong>
                     <p>Your trusty, reliable blade. No special effects.</p>
                 </div>
 
-                {/* Map over the weapons data and only show the ones the player has unlocked */}
                 {weapons.map(w => (
                     unlockedWeapons[w.id] && (
                         <div
                             key={w.id}
                             className={`weapon-item ${equippedWeapon === w.id ? 'equipped' : ''}`}
-                            onClick={() => handleEquipWeapon(w.id)}
+                            onClick={() => useGameStore.getState().handleEquipWeapon(w.id)}
                         >
                             <strong>{w.name}</strong>
                             <p>{w.description}</p>
