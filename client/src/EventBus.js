b969c2187613fe3884159282521a6018d044a6b2
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 20:58:43 -0700

    Update EventBus.js

--- client/src/EventBus.js
+++ client/src/EventBus.js
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 
-// Create a new Phaser Event Emitter instance and export it
 const EventBus = new Phaser.Events.EventEmitter();
+
+// ✨ NEW: Add a unique ID to this specific instance
+EventBus.id = Date.now() + Math.random();
+console.log(`EventBus instance created with ID: ${EventBus.id}`);
 
 export default EventBus;
